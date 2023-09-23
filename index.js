892f956fe8ac97e8be1e4d92fcdd6d7939e1dfb2
toma2023 <mrinmoyehimadrita@gmail.com>
2023-09-23 15:33:36 +0600

    updated

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const cors = require("cors");
 
 require("dotenv").config();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hgsaz0s.mongodb.net/?retryWrites=true&w=majority`;
 const uri =
   "mongodb+srv://trio_Dreamer:vV3AtBC6CnPIpD4u@cluster0.hgsaz0s.mongodb.net/?retryWrites=true&w=majority";
@@ -41,16 +41,30 @@ async function run() {
       // Calculate the date 30 days ago from the current date
       const thirtyDaysAgo = new Date();
       thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
-      
+
       const latestProducts = await productsCollections
-      .find({ submission_date: { $gte: thirtyDaysAgo.toISOString().split('T')[0] } })
-      .sort({ submission_date: -1 })
-      .limit(10) // You can adjust the limit as needed
-      .toArray();
+        .find({
+          submission_date: { $gte: thirtyDaysAgo.toISOString().split("T")[0] },
+        })
+        .sort({ submission_date: -1 })
+        .limit(10) // You can adjust the limit as needed
+        .toArray();
 
-console.log(latestProducts)
+      res.send(latestProducts);
+    });
 
-    res.send({});
+    //single product
+    app.get("/singleProduct/:id", async (req, res) => {
+      const id = req.params.id;
+    
+      // Check if the id is a valid ObjectId before using it
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).send({ error: true, message: 'Invalid ObjectId' });
+      }
+    
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollections.findOne(query);
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
